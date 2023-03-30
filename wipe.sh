b0ad596ba29c9e6f36a632fa2e386b9523a48bc0
mamajune2022 <125930294+mamajune2022@users.noreply.github.com>
2023-03-30 13:33:47 -0400

    Update wipe.sh

--- wipe.sh
+++ wipe.sh
@@ -6,18 +6,20 @@ folder="/Users/user/folder"
 # Set the maximum percentage of disk space to use
 max_percent=95
 
+# Generate a random string and write it to a file in the folder
+random_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+
 # Loop until the folder size exceeds the maximum percentage of disk space
 while true; do
     # Get the current disk usage percentage
-    size=$(df / | tail -1 | awk '{print $5}' | tr -d '%')
+    size=$(df / | tail -1 | tr -d '%' | awk '{print $5}')
     
     # If the disk usage percentage is greater than or equal to the maximum percentage, delete the folder and start over
     if [ $size -ge $max_percent ]; then
         rm -rf $folder
         mkdir $folder
     else
-        # Generate a random string and write it to a file in the folder
-        random_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+        # Write it to a file in the folder
         echo $random_string >> $folder/random
         # Update the folder size
         size=$(du -s $folder | awk '{print $1}')
